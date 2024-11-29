1307e51606a7b6b19a932fa66554c60a98487b3d
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2024-11-29 15:06:01 +0300

    writing the main.py code to excute all funcs  --1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #THis Is the Main file for this project.
+
+#imports
+
+import func as f 
+
+
+
+#Main code to run program
+
+ch = ["create account","deposit","withdraw"]
+
+Choice = input(f"What do you want to do {ch} : \n").lower()
+
+if Choice in ch:
+    if Choice == "create account":
+        f.create_account()
+    elif Choice == "deposit":
+        f.deposit_money()
+    elif Choice == "withdraw":
+        f.withdraw_money()
+elif Choice not in ch:
+    print("Invalid Choice")
+    print("pls retry")
