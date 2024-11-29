d572fee60f54df6ccda69d973af30802a0c8d0ac
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2024-11-29 14:02:12 +0300

    create a create_account func code  -1

--- func.py
+++ func.py
@@ -5,18 +5,34 @@
 
 #creating a account
 def create_account():
-    pass
+    global account_number,customer_name
+    customer_name = input("PLs enter you name: \n")
+    account_number = int(input("pls enter a new account number to create one: \n"))
+    default_deposit = int(input("Pls make a deposit : \n"))
+    with open(f"{account_number} logs","w") as c_a:
+        c_a.write(f"This account belongs to {customer_name} ")
+
+    with open(f"{account_number} logs","a") as c_a:
+         c_a.write(f"default deposit is {default_deposit}")
+
+    with open(account_number,"w") as c_a:
+        c_a.write(f"x = {default_deposit}")
+
+
 
 #checking balance
 def check_balance():
-    pass
+    global account_number,customer_name
+
 
 #deposit moneu
 def deposit_money():
-    pass
+    global account_number,customer_name
+
 
 #withdraw money
 def withdraw_money():
-    pass
+    global account_number,customer_name
+
 
 #END
