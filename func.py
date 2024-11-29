627b95ceea3e9821c901c249c513199c71e98a07
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2024-11-29 13:34:26 +0300

    making a outline of all main functions.

--- func.py
+++ func.py
@@ -1,1 +1,18 @@
 #This file is for func in this program.
+
+
+# Functions
+
+#checking balance
+def check_balance():
+    pass
+
+#deposit moneu
+def deposit_money():
+    pass
+
+#withdraw money
+def withdraw_money():
+    pass
+
+#END
